80e44035b67bc29f97cc5bd8a279fc4fbc13684b
Cian Dunne <20091471@mail.wit.ie>
2021-05-22 17:23:19 +0100

    couple of small changes - just made to allow communication with svelte app

--- test/poi-service.js
+++ test/poi-service.js
@@ -1,41 +1,12 @@
 "use strict";
 
 const axios = require("axios");
-const baseUrl = "http://localhost:3000";
+const baseUrl = "http://localhost:4000";
 
 class poiService {
   constructor(baseUrl) {
     this.baseUrl = baseUrl;
   }
-
-  /*async getPois() {
-    const response = await axios.get(this.baseUrl + "/api/pois");
-    return response.data;
-  }
-
-  async getPoi(id) {
-    try {
-      const response = await axios.get(this.baseUrl + "/api/pois/" + id);
-      return response.data;
-    } catch (e) {
-      return null;
-    }
-  }
-
-  async createPoi(newPoi) {
-    const response = await axios.post(this.baseUrl + "/api/pois", newPoi);
-    return response.data;
-  }
-
-  async deleteAllPois() {
-    const response = await axios.delete(this.baseUrl + "/api/pois");
-    return response.data;
-  }
-
-  async deleteOnePoi(id) {
-    const response = await axios.delete(this.baseUrl + "/api/pois/" + id);
-    return response.data;
-  }*/
 
   async getUsers() {
     const response = await axios.get(this.baseUrl + "/api/users");
